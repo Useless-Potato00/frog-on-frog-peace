20054462af43680ba6132674890470e3ff425176
Fredrick Square <79672214+Useless-Potato00@users.noreply.github.com>
2022-07-05 01:59:40 +0000

    k

---
+++ frog.py
@@ -0,0 +1,8 @@
+answerone = int(input("How many frogs do you own per pond? "))
+answertwo = int(input("How many ponds do you own? "))
+
+def cal():
+    x = answerone * answertwo
+    return x
+x = cal()
+print(x)
